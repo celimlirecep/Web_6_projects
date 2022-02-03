4c2c262ad6c1f4c2a48825b8357d759f1058cd3f
Recep Çelimli <receply13@hotmail.com>
2022-02-03 12:45:29 +0300

    3.02.2022 proje_08 i bitiridim

---
+++ proje_08_For/proje_08_For/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace proje_08_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  int sum = 0;
+              for (int i = 1; i < 11; i+=2)
+              {
+                  *//*if (i%2==0)
+                  {
+                      Console.WriteLine($"i degeri: {i}");
+                      sum += i;
+
+                  }*//*
+
+                  Console.WriteLine($"i degeri: {i}");
+                  sum += i;
+              }
+              Console.WriteLine( $"sum: {sum}");*/
+
+            // 1 100 arasindaki asal sayiler
+            //  bool deger = false;
+
+            /*for (int i = 2; i < 101; i++)
+            {
+                for (int j = i-1; j >1; j--)
+                {
+                    if (i%j==0 )
+                    {
+                        deger = true;
+                        break;
+                    }
+                }
+
+                if(!deger)
+                    {
+                    Console.WriteLine(i);
+                }
+                deger = false;
+            }*/
+
+            // 1 100 arasında 5 e ve 7 ye tam bölünemeyneler
+            /*
+                        for (int i = 1; i < 101; i++)
+                        {
+                            if (i%5==0 && i%7==0)
+                            {
+                                Console.WriteLine(i);
+                            }
+
+                        }*/
+
+            /* for (int i = 1; i < 6; i++)
+             {
+                 for (int j = 1; j < 6; j++)
+                 {
+                     Console.Write("*");
+                 }
+                 Console.WriteLine();
+
+             }*/
+
+
+            /*      int yourNumber=0;
+                  int comNumber=0;
+                  do
+                  {
+                      Console.Write("enter your number: ");
+                       yourNumber = int.Parse(Console.ReadLine());
+                      Random rastgele = new Random();
+                       comNumber = rastgele.Next(1, 10);
+                      Console.WriteLine($"your number:{yourNumber}  computer number:{comNumber} ");
+
+                  } while (yourNumber != comNumber);*/
+
+            //çarpım tablosu
+
+            /* for (int i = 1; i <11 ; i++)
+             {
+                 for (int j = 1; j < 11; j++)
+                 {
+                     Console.Write($" {j*i}\t");
+
+                 }
+                 Console.WriteLine();
+             }
+
+             //klavye 3 adet girilen sayıdan en büyük sayiyi ekrana yazdirİ;
+
+
+ */
+
+
+
+            /*  int bigNUmber = 0;
+              int sayac = 1;
+              while (sayac!=4)
+              {
+                  sayac++;
+                  Console.Write($"{sayac-1}. sayiyi giriniz:");
+                  int userNumber = int.Parse(Console.ReadLine());
+                  if (bigNUmber < userNumber)
+                  {
+                      bigNUmber = userNumber;
+                  }
+
+              }
+              Console.WriteLine("en büyük sayiniz: " +bigNUmber);*/
+            int sayac = 1;
+            int bigNumber = 0;
+            while (sayac!=3)
+            {
+                Console.Write(sayac+" sayiniz:  ");
+                sayac++;
+                int userName=int.Parse(Console.ReadLine());
+                Random rastgele = new Random();
+                int rasgeleNumber = rastgele.Next(1,100);
+                if (bigNumber<rasgeleNumber)
+                {
+                    bigNumber = rasgeleNumber;
+                }
+
+
+            }
+            Console.WriteLine("girilen sayilardan en buyugu: " + bigNumber);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
