7cd179c9a5947f392db6fe26c2d709383833eaec
Recep Çelimli <98876147+celimlirecep@users.noreply.github.com>
2022-02-05 18:48:40 +0300

    ders tekrari

---
+++ proje_11_method_example_2/Proje_11_method_example_2_study/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Proje_11_method_example_2_study
+{
+    class Program      //daienin alanını bulan pi, bazen 3 bazende 3,14 olan kodu yazin
+    {
+        static double DaireAlani(int yariCap,int piDegeri)
+        {
+            double alan = piDegeri * (yariCap ^ 2);
+
+            return alan;
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] daireBilgileri = { 4, 15,15,48,86 };
+
+
+        }
+    }
+}
--- proje_11_method_example_2/proje_11_method_example_2/Program.cs
+++ proje_11_method_example_2/proje_11_method_example_2/Program.cs
@@ -4,7 +4,7 @@ namespace proje_11_method_example_2
 {
     class Program
     {
-        static int enBuyuk(int[] sayilar)
+        static int EnBuyuk(int[] sayilar)
         {
             int enB = 0;
             for (int i = 0; i < sayilar.Length; i++)
@@ -17,20 +17,21 @@ namespace proje_11_method_example_2
             }
             return enB;
         }
-        static double alan(int) 
+        
         static void Main(string[] args)
         {
-            /*// gönderilen 2 sayidan hangisi daha büyük bulan program
+            // gönderilen 2 sayidan hangisi daha büyük bulan program
 
 
             int[] sayilar = { 15, 76,87 ,1,465};
-            Console.WriteLine(enBuyuk(sayilar));
+            Console.WriteLine(EnBuyuk(sayilar));
             Console.ReadLine();
 
+            
+           
+        }
 
-        }*/
 
-        //daienin alanını bulan pi bazen 3 bazende 3,14 olan kodu yazin
 
     }
 }
